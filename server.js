57903a3bc0c12a77ed5547ca59ff46dca10c0010
nelsa01 <n.ishimwe@alustudent.com>
2022-08-27 13:52:23 +0200

    retry cors

--- server.js
+++ server.js
@@ -16,14 +16,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 // Setup cors origin to allow calls from only our frontend
 app.use(cors({
+  origin: 'https://reddit-frontend-dkkydt16f-nelsa01.vercel.app',
   credentials: true,
 }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Origin', 'https://reddit-frontend-dkkydt16f-nelsa01.vercel.app');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-  });
+});
 
 app.use(VotingRoutes);
 
